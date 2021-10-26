accbf77aa8a1d56b04cde4ec6acf20edb0d2e4e2
MateuszK <mateusz.kulawiak98@gmail.com>
2021-10-26 17:38:14 +0200

    Add inputs for controls some props

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,82 @@
 <template>
-  <div>
+  <div class="app">
     <base-form
+      v-if="!showControls"
+      ref="form"
       class="reservation-form"
       :price="price"
       :stars="stars"
       :currency="currency"
       :selectedDates="selectedDates"
       :disabledDates="disabledDates"
+      :activeDatesFrom="activeDatesFrom"
+      :activeDatesTo="activeDatesTo"
+      :opinionsNumber="opinionsNumber"
+      :formatDates="formatDates"
     />
+    <button class="controls-bttn" @click="showControls = !showControls">
+      {{ showControls ? "Hide controls" : "Show controls" }}
+    </button>
+    <div class="controls" v-if="showControls">
+      Control props:
+      <input-text v-model="currency" label="Currency" />
+      <input-text v-model="formatDates" label="date format:" />
+      <input-text v-model="activeDatesFrom" label="active dates from:" />
+      <input-text v-model="activeDatesTo" label="active dates to:" />
+      <input-text
+        @clickBtn="addDisabledDateToList"
+        with-bttn
+        v-model="disabledDateToAdd"
+        label="Add disabled date:"
+      />
+      <div>
+        Disabled dates <br />
+        (Remove disabled date by click):
+        <span
+          v-for="(item, idx) in disabledDates"
+          :key="idx"
+          @click="deleteDisabledDateFormList(item)"
+          ><br />{{ item }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BaseForm from "@/components/BaseForm";
+import InputText from "@/components/controls/InputText";
 export default {
   name: "App",
   components: {
     BaseForm,
+    InputText,
   },
   data: () => ({
+    isVisible: true,
+    formatDates: "DD.MM.YYYY",
     stars: 4.5,
     price: 298,
     currency: "zł",
-    // TODO: Handle different format
-    selectedDates: ["2021.10.20", "2021.10.13"],
-    disabledDates: ["2021.10.08"],
+    selectedDates: [],
+    activeDatesFrom: undefined,
+    opinionsNumber: 298,
+    activeDatesTo: undefined,
+    disabledDates: [],
+
+    disabledDateToAdd: "",
+    showControls: false,
   }),
+  methods: {
+    deleteDisabledDateFormList(item) {
+      const idxItem = this.disabledDates.indexOf((date) => date === item);
+      this.disabledDates.splice(idxItem, 1);
+    },
+    addDisabledDateToList() {
+      this.disabledDates.push(this.disabledDateToAdd);
+      this.disabledDateToAdd = "";
+    },
+  },
 };
 </script>
 
@@ -35,5 +86,25 @@ export default {
   margin-top: 2 * $gap;
   margin-left: auto;
   margin-right: auto;
+}
+.controls {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid $gray;
+  padding: 0.25 * $gap;
+  div {
+    margin-bottom: 0.5 * $gap;
+    margin-top: 0.5 * $gap;
+  }
+}
+.app {
+  height: 100vh;
+}
+.controls-bttn {
+  position: fixed;
+  top: 10px;
+  left: 10px;
 }
 </style>
---
+++ src/components/controls/InputText.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label class="label" for="input-text">{{ label }}</label>
+    <input
+      type="text"
+      id="input-text"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+    <button v-if="withBttn" @click="$emit('clickBtn')">{{ bttnText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+    bttnText: {
+      type: String,
+      default: "Click",
+    },
+    withBttn: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.label {
+  margin-right: 0.5 * $gap;
+}
+</style>
